d95b21135d737708645b611f852d20a3446a5a68
Wagner CS Filho <wagnercsfilho@hotmail.com>
2014-07-30 18:29:24 -0300

    Config database

--- app.js
+++ app.js
@@ -4,6 +4,31 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+mysql      = require('mysql');
+
+// Application initialization
+var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : ''
+    });
+
+// Database setup
+connection.query('CREATE DATABASE IF NOT EXISTS SlideShow', function (err) {
+    if (err) throw err;
+    connection.query('USE SlideShow', function (err) {
+        if (err) throw err;
+        connection.query('CREATE TABLE IF NOT EXISTS photos('
+            + 'id INT NOT NULL AUTO_INCREMENT,'
+            + 'PRIMARY KEY(id),'
+            + 'title VARCHAR(50)'
+            + 'description VARCHAR(255)'
+            + 'path VARCHAR(255)'
+            +  ')', function (err) {
+                if (err) throw err;
+            });
+    });
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
